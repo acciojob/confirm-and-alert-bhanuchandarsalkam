e93d65f4646054f9b07f6e3d1261700eb1ff15bb
bhanuchandarsalkam <133007383+bhanuchandarsalkam@users.noreply.github.com>
2023-09-24 15:57:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-//your JS code here. If required.
-confirm("Do you want to proceed?")
+
+const user=confirm("Do you want to proceed?");
+if(user){
+	alert("You clicked OK. Proceeding...")
+}
+else{
+	alert("You clicked Cancel. Exiting...")
+}
